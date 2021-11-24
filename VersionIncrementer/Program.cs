e542b41024332a98fba6792a586c5f27d6622a05
pauldendulk <pauldendulk@gmail.com>
2021-11-24 09:16:15 +0100

    Parse increment from command line argument

--- VersionIncrementer/Program.cs
+++ VersionIncrementer/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using VersionIncrementer.Extensions;
 
 namespace VersionIncrementer
 {
+
+    public class Options
+    {
+        [Option('i', "increment", Required = true, HelpText = "Specifies the type of increment (MAJOR|MINOR|PATCH)")]
+        public string Type { get; set; } = default!;
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(o =>
+                {
+                    var increment = IncrementParser.Parse(o.Type);
+                    Console.WriteLine($"Increment Type: {increment}");
+                })
+                .WithNotParsed(HandleParseError);
+        }
+
+        private static void HandleParseError(IEnumerable<Error> errors)
+        {
+            Console.WriteLine("Parse error");
+
+            foreach (var error in errors)
+            {
+                Console.WriteLine(error.Tag);
+            }
         }
     }
 }
